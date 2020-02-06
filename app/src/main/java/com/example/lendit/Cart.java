b7b60811ab9f17df35b75c2abe8042548716cc26
sony <sonym3@gmail.com>
2020-02-05 23:45:08 -0500

    basic ui for dasboard with bottom nav bar

--- app/src/main/java/com/example/lendit/Cart.java
+++ app/src/main/java/com/example/lendit/Cart.java
@@ -1,44 +1,46 @@
 package com.example.lendit;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Dashboard extends AppCompatActivity {
+public class Cart extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_cart);
+
+
+
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
-        bottomNavigationView.setSelectedItemId(R.id.navigation_home );
+        bottomNavigationView.setSelectedItemId(R.id.navigation_cart );
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
-                        break;
-                    case R.id.navigation_cart:
-                        Intent a = new Intent(Dashboard.this,Cart.class);
+                        Intent a = new Intent(Cart.this,Dashboard.class);
                         startActivity(a);
                         overridePendingTransition(0,0);
                         return true;
+                    case R.id.navigation_cart:
+                        break;
                     case R.id.navigation_profile:
-                        Intent b = new Intent(Dashboard.this, Profile.class);
+                        Intent b = new Intent(Cart.this, Profile.class);
                         startActivity(b);
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.navigation_waitlist:
-                        Intent c = new Intent(Dashboard.this,WaitList.class);
+                        Intent c = new Intent(Cart.this,WaitList.class);
                         startActivity(c);
                         overridePendingTransition(0,0);
                         return true;
--- app/src/main/java/com/example/lendit/WaitList.java
+++ app/src/main/java/com/example/lendit/WaitList.java
@@ -1,47 +1,47 @@
 package com.example.lendit;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Dashboard extends AppCompatActivity {
+public class WaitList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_wait_list);
+
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
-        bottomNavigationView.setSelectedItemId(R.id.navigation_home );
+        bottomNavigationView.setSelectedItemId(R.id.navigation_waitlist );
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
-                        break;
-                    case R.id.navigation_cart:
-                        Intent a = new Intent(Dashboard.this,Cart.class);
+                        Intent a = new Intent(WaitList.this,Dashboard.class);
                         startActivity(a);
                         overridePendingTransition(0,0);
                         return true;
+                    case R.id.navigation_cart:
+                        Intent c = new Intent(WaitList.this,Cart.class);
+                        startActivity(c);
+                        overridePendingTransition(0,0);
+                        return true;
                     case R.id.navigation_profile:
-                        Intent b = new Intent(Dashboard.this, Profile.class);
+                        Intent b = new Intent(WaitList.this, Profile.class);
                         startActivity(b);
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.navigation_waitlist:
-                        Intent c = new Intent(Dashboard.this,WaitList.class);
-                        startActivity(c);
-                        overridePendingTransition(0,0);
-                        return true;
+                        break;
                 }
                 return false;
             }
